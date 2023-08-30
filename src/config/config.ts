e616eb92ee9bee096c90d939e7d547b56d72682b
AmeerHamXa1212 <ameer.hamza@ocloudsolutions.net>
2023-08-30 16:28:16 +0500

    Atlas connection

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,5 @@
 export const appConfig = {
   port: parseInt(process.env.PORT || "5500", 10),
-  databaseURL: "mongodb://127.0.0.1:27017/HospitalAPI-ahk",
+  databaseURL:
+    "mongodb+srv://ameerhamza:ameerhamza11@cluster0.e99r2go.mongodb.net/",
 };
