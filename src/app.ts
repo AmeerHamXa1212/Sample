c6ac79dc79a0cb4cc25f9fa68619d5c0642edb74
AmeerHamXa1212 <ameer.hamza@ocloudsolutions.net>
2023-08-30 17:06:53 +0500

    dot env added

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,23 @@ import express from "express";
 import { appConfig } from "./config/config";
 import routes from "./routes/index";
 import * as mongoose from "mongoose";
+import * as dotenv from "dotenv";
 import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
+dotenv.config();
+
+const databaseURL =
+  process.env.dbUrl ||
+  "mongodb+srv://ameerhamza:ameerhamza11@cluster0.e99r2go.mongodb.net/";
+const port = process.env.port || 5500;
 
 app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
 
 mongoose
-  .connect(appConfig.databaseURL, {
+  .connect(databaseURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   } as mongoose.ConnectOptions)
