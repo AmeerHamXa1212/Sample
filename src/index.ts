7b5dfa199bd096a8641485f078f42777f7778d20
AmeerHamXa1212 <ameer.hamza@ocloudsolutions.net>
2023-08-30 18:37:59 +0500

    c1

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { appConfig } from "./config/config";
 import mongoose from "mongoose";
 
 const port = appConfig.port;
+console.log(port);
 mongoose.set("strictQuery", true);
 const server = app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
